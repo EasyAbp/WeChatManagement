d17c80c6e6e9ca2427db33203f781e4e2b89f3f3
gdlcf88 <gdlcf88@outlook.com>
2020-07-01 00:53:20 +0800

    完善LoginDto的remark

--- modules/MiniPrograms/src/EasyAbp.WeChatManagement.MiniPrograms.Application.Contracts/EasyAbp/WeChatManagement/MiniPrograms/Dtos/LoginDto.cs
+++ modules/MiniPrograms/src/EasyAbp.WeChatManagement.MiniPrograms.Application.Contracts/EasyAbp/WeChatManagement/MiniPrograms/Dtos/LoginDto.cs
@@ -39,9 +39,10 @@ namespace EasyAbp.WeChatManagement.MiniPrograms.Dtos
         /// <remark>
         /// 若不传入此值，可能导致无法获取用户的 unionId，
         /// 一般情况下建议传值，符合以下“任一”条件可不传入：
-        /// 1. 明确：此用户（在当前小程序加入开放平台后）曾授权登录过“当前”小程序。
+        /// 1. 明确：此用户（在当前小程序加入开放平台后）曾授权登录过“当前”小程序，且登录时使用的是本接口。
         /// 2. 明确：此用户在同开放平台+同主体下，关注过“任意”公众号。
         /// 3. 明确：此用户在同开放平台+同主体下，登录过“其他任意” app。
+        /// 4. 明确：当前 app 没有加入开放平台，或不需要使用 UnionId 作为用户识别凭证。
         /// 更多信息：https://developers.weixin.qq.com/miniprogram/dev/framework/open-ability/union-id.html
         /// </remark>
         public string EncryptedData { get; set; }
@@ -52,9 +53,10 @@ namespace EasyAbp.WeChatManagement.MiniPrograms.Dtos
         /// <remark>
         /// 若不传入此值，可能导致无法获取用户的 unionId，
         /// 一般情况下建议传值，符合以下“任一”条件可不传入：
-        /// 1. 明确：此用户（在当前小程序加入开放平台后）曾授权登录过“当前”小程序。
+        /// 1. 明确：此用户（在当前小程序加入开放平台后）曾授权登录过“当前”小程序，且登录时使用的是本接口。
         /// 2. 明确：此用户在同开放平台+同主体下，关注过“任意”公众号。
         /// 3. 明确：此用户在同开放平台+同主体下，登录过“其他任意” app。
+        /// 4. 明确：当前 app 没有加入开放平台，或不需要使用 UnionId 作为用户识别凭证。
         /// 更多信息：https://developers.weixin.qq.com/miniprogram/dev/framework/open-ability/union-id.html
         /// </remark>
         public string Iv { get; set; }
