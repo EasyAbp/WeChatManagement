68789e5ea8f08551f5f1cbe0450afdd4115f6874
gdlcf88 <gdlcf88@outlook.com>
2020-11-30 16:38:45 +0800

    Add "EasyAbp" prefix to DbTablePrefix and ConnectionStringName

--- modules/MiniPrograms/src/EasyAbp.WeChatManagement.MiniPrograms.Domain/EasyAbp/WeChatManagement/MiniPrograms/MiniProgramsDbProperties.cs
+++ modules/MiniPrograms/src/EasyAbp.WeChatManagement.MiniPrograms.Domain/EasyAbp/WeChatManagement/MiniPrograms/MiniProgramsDbProperties.cs
@@ -2,10 +2,10 @@
 {
     public static class MiniProgramsDbProperties
     {
-        public static string DbTablePrefix { get; set; } = "WeChatManagementMiniPrograms";
+        public static string DbTablePrefix { get; set; } = "EasyAbpWeChatManagementMiniPrograms";
 
         public static string DbSchema { get; set; } = null;
 
-        public const string ConnectionStringName = "WeChatManagementMiniPrograms";
+        public const string ConnectionStringName = "EasyAbpWeChatManagementMiniPrograms";
     }
 }
