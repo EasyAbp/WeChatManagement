b94b0623c116f9afa18e16ca9c1d3379fff6d1e8
Slarkerino Company Desktop <307221214@qq.com>
2021-01-22 17:14:09 +0800

    给异常增加状态码

--- modules/MiniPrograms/src/EasyAbp.WeChatManagement.MiniPrograms.Application/EasyAbp/WeChatManagement/MiniPrograms/Login/PcLoginAuthorizeTooFrequentlyException.cs
+++ modules/MiniPrograms/src/EasyAbp.WeChatManagement.MiniPrograms.Application/EasyAbp/WeChatManagement/MiniPrograms/Login/PcLoginAuthorizeTooFrequentlyException.cs
@@ -4,7 +4,7 @@ namespace EasyAbp.WeChatManagement.MiniPrograms.Login
 {
     public class PcLoginAuthorizeTooFrequentlyException : UserFriendlyException
     {
-        public PcLoginAuthorizeTooFrequentlyException() : base("授权登录操作太过频繁，请稍后重试")
+        public PcLoginAuthorizeTooFrequentlyException() : base("授权登录操作太过频繁，请稍后重试", "PcLoginAuthorizeTooFrequently")
         {
             
         }
