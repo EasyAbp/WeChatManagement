8f0a7d165ca51e6ab42724963fc974bfa0324654
gdlcf88 <gdlcf88@outlook.com>
2023-03-23 20:30:01 +0800

    Upgrade to ABP 7.1.0

--- samples/WeChatManagementSample/aspnet-core/src/WeChatManagementSample.EntityFrameworkCore/Migrations/20230323122921_UpgradedToAbp_7_1.Designer.cs
+++ samples/WeChatManagementSample/aspnet-core/src/WeChatManagementSample.EntityFrameworkCore/Migrations/20230323122921_UpgradedToAbp_7_1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 using WeChatManagementSample.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using WeChatManagementSample.EntityFrameworkCore;
 namespace WeChatManagementSample.Migrations
 {
     [DbContext(typeof(WeChatManagementSampleDbContext))]
-    partial class WeChatManagementSampleDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230323122921_UpgradedToAbp_7_1")]
+    partial class UpgradedToAbp71
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
