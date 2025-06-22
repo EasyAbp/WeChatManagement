11446f6bb6d6c190bab683f6679cb6715fc5ced8
slarkerino <wanjackcool@qq.com>
2025-06-22 21:41:58 +0800

    doc: Enhance Comment
    Updated LoginInput.cs and documentation to clarify and support user lookup by phone number when openId/unionId matching fails. The system now attempts to retrieve and match users by phone number using the PhoneNumberCode parameter, improving login flexibility and user experience.

--- modules/MiniPrograms/src/EasyAbp.WeChatManagement.MiniPrograms.Application.Contracts/EasyAbp/WeChatManagement/MiniPrograms/Login/Dtos/LoginInput.cs
+++ modules/MiniPrograms/src/EasyAbp.WeChatManagement.MiniPrograms.Application.Contracts/EasyAbp/WeChatManagement/MiniPrograms/Login/Dtos/LoginInput.cs
@@ -20,8 +20,11 @@ namespace EasyAbp.WeChatManagement.MiniPrograms.Login.Dtos
         public string Code { get; set; }
 
         /// <summary>
-        /// 通过微信开放能力 getRealtimePhoneNumber 获取的动态令牌，用于换取用户手机号
-        /// 可用于查找已有用户绑定，避免重复创建用户
+        /// 通过微信开放能力 getRealtimePhoneNumber 获取的动态令牌
+        /// 当通过 openId/unionId 查找用户失败时，如提供此值，应用服务将使用该令牌作为备选方案：
+        /// 1. 尝试换取用户手机号
+        /// 2. 通过手机号查找已注册用户
+        /// 3. 若找到匹配用户，则直接使用该用户登录；若未找到，则创建新用户并关联此手机号
         /// https://developers.weixin.qq.com/miniprogram/dev/framework/open-ability/getRealtimePhoneNumber.html
         /// </summary>
         [CanBeNull]
